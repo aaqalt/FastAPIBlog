a7a53af55ad309d96cb7c00a64de8c712aeddfbc
aaqalt <abrorbek.axmadjanov810@gmail.com>
2025-11-24 11:46:03 +0500

    adding env

--- blog/database.py
+++ blog/database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-DB_URL ='postgresql://postgres:0810@localhost:5433/blogsfast'
+DB_URL =os.getenv("DATABASE_URL")
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
